5fe29eb70acb64c90ca58e2d3aa43df73824698d
Abhik <abhiksetia@gmail.com>
2017-10-16 11:43:23 +0530

    designed popup.html

--- background.js
+++ background.js
@@ -2,15 +2,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    chrome.tabs.sendMessage(activeTab.id, {"message": "get_links"});
   });
 });
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	if(request.message==="open_new_tab"){
-  		chrome.tabs.create({"url":request.url});
-  	}
+  function(response, sender, sendResponse) {
+  	alert(response)
   });
 
 
