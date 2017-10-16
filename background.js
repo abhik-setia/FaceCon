0cdab42f275304e6f6e64ad9dcbf66057be9b988
Abhik <abhiksetia@gmail.com>
2017-10-16 23:58:22 +0530

    Extracted twitter data part 1

--- background.js
+++ background.js
@@ -1,14 +1,43 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "get_links"});
-  });
+
+//vars
+var html_data,profile_data,tweets,following,followers,lists,moments;
+
+$(function () {
+	 $.get("https://twitter.com/abhik_setia",function (data) {
+	 	html_data=data;
+	 	profile_data=$(html_data).find('.ProfileNav-list');
+	 	
+	 	//Twitter Data
+	 	tweets=$(profile_data).find('span.ProfileNav-value').eq(0).text();
+	 	following=$(profile_data).find('span.ProfileNav-value').eq(1).text();
+	 	followers=$(profile_data).find('span.ProfileNav-value').eq(2).text();
+	 	lists=$(profile_data).find('span.ProfileNav-value').eq(3).text();
+	 	moments=$(profile_data).find('span.ProfileNav-value').eq(4).text();
+
+	 	//sidebar data
+	 	profile_sidebar_data=$(html_data).find('.ProfileSidebar');
+	 	bio=$(profile_sidebar_data).find('.ProfileHeaderCard-bio').text();
+	 	location_data=$(profile_sidebar_data).find('.ProfileHeaderCard-locationText').text();
+	 	joinDate=$(profile_sidebar_data).find('.ProfileHeaderCard-joinDateText').text();
+	 	birthDatetext=$(profile_sidebar_data).find('.ProfileHeaderCard-birthdateText').text();
+
+	 	
+
+	 }); 
 });
 
-chrome.runtime.onMessage.addListener(
-  function(response, sender, sendResponse) {
-  	alert(response)
-  });
-
-
+function getTweets () {
+	return tweets;
+}
+function getFollowing () {
+	return following;
+}
+function getFollowers () {
+	return followers;
+}
+function getLists () {
+	return lists;
+}
+function getMoments () {
+	return moments;
+}
